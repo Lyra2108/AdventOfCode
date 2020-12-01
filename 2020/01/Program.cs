3b38beb3d05881e385241e9460639a97eb32467a
Lyra <git@sblume.eu>
2020-12-01 23:10:04 +0100

    Day1 2020

---
+++ 2020/01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = InputNumbers().OrderBy(item => item).ToList();
+            FindTwoNumbers(numbers);
+
+            foreach (var firstNumber in numbers)
+            {
+                foreach (var firstSum in numbers.Select(number => number + firstNumber).Where(sum => sum < 2020))
+                {
+                    var thirdNumber = 2020 - firstSum;
+                    if (numbers.Contains(thirdNumber))
+                    {
+                        var secoundNumber = firstSum - firstNumber;
+                        Console.Out.WriteLine($"The numbers are {firstNumber}, {secoundNumber} and {thirdNumber}. The result is {firstNumber * secoundNumber * thirdNumber}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void FindTwoNumbers(List<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                var otherNumber = 2020 - number;
+                if (numbers.Contains(otherNumber))
+                {
+                    Console.Out.WriteLine($"The numbers are {number} and {otherNumber}. The result is {number * otherNumber}");
+                    break;
+                }
+            }
+        }
+
+        private static IEnumerable<int> InputNumbers()
+        {
+            using var fileReader = new StreamReader("input.txt");
+            string line;
+            while((line = fileReader.ReadLine()) != null){
+                yield return int.Parse(line);
+            }
+        }
+    }
+}
