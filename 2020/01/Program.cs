423f7ee7f6ec8bc07b4bf388ca65fdf392694001
Lyra <git@sblume.eu>
2020-12-02 20:11:53 +0100

    Day 2

--- 2020/01/Program.cs
+++ 2020/01/Program.cs
@@ -3,50 +3,41 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _01
+var numbers = InputNumbers().OrderBy(item => item).ToList();
+FindTwoNumbers(numbers);
+
+foreach (var firstNumber in numbers)
 {
-    class Program
+    foreach (var firstSum in numbers.Select(number => number + firstNumber).Where(sum => sum < 2020))
     {
-        static void Main(string[] args)
+        var thirdNumber = 2020 - firstSum;
+        if (numbers.Contains(thirdNumber))
         {
-            var numbers = InputNumbers().OrderBy(item => item).ToList();
-            FindTwoNumbers(numbers);
-
-            foreach (var firstNumber in numbers)
-            {
-                foreach (var firstSum in numbers.Select(number => number + firstNumber).Where(sum => sum < 2020))
-                {
-                    var thirdNumber = 2020 - firstSum;
-                    if (numbers.Contains(thirdNumber))
-                    {
-                        var secoundNumber = firstSum - firstNumber;
-                        Console.Out.WriteLine($"The numbers are {firstNumber}, {secoundNumber} and {thirdNumber}. The result is {firstNumber * secoundNumber * thirdNumber}");
-                        return;
-                    }
-                }
-            }
+            var secoundNumber = firstSum - firstNumber;
+            Console.Out.WriteLine($"The numbers are {firstNumber}, {secoundNumber} and {thirdNumber}. The result is {firstNumber * secoundNumber * thirdNumber}");
+            return;
         }
+    }
+}
 
-        private static void FindTwoNumbers(List<int> numbers)
+static void FindTwoNumbers(List<int> numbers)
+{
+    foreach (var number in numbers)
+    {
+        var otherNumber = 2020 - number;
+        if (numbers.Contains(otherNumber))
         {
-            foreach (var number in numbers)
-            {
-                var otherNumber = 2020 - number;
-                if (numbers.Contains(otherNumber))
-                {
-                    Console.Out.WriteLine($"The numbers are {number} and {otherNumber}. The result is {number * otherNumber}");
-                    break;
-                }
-            }
+            Console.Out.WriteLine($"The numbers are {number} and {otherNumber}. The result is {number * otherNumber}");
+            break;
         }
+    }
+}
 
-        private static IEnumerable<int> InputNumbers()
-        {
-            using var fileReader = new StreamReader("input.txt");
-            string line;
-            while((line = fileReader.ReadLine()) != null){
-                yield return int.Parse(line);
-            }
-        }
+static IEnumerable<int> InputNumbers()
+{
+    using var fileReader = new StreamReader("input.txt");
+    string line;
+    while((line = fileReader.ReadLine()) != null){
+        yield return int.Parse(line);
     }
 }
---
+++ 2020/02/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+var input = ReadInLines();
+var regex = new Regex(@"(?'min'\d+)-(?'max'\d+) (?'char'\w): (?'password'\w+)");
+
+var validPasswords = input.Count(line =>
+{
+    var match = regex.Match(line);
+    var rule = new TimesPasswordPolicy(ParseInt(match, "min"), ParseInt(match, "max"), match.Groups["char"].Value.ToCharArray()[0]);
+
+    return rule.IsValid(match.Groups["password"].Value);
+});
+
+Console.WriteLine($"There are {validPasswords} with the times policy.");
+
+var valid2ndRulePasswords = input.Count(line =>
+{
+    var match = regex.Match(line);
+    var rule = new PositionPasswordPolicy(ParseInt(match, "min"), ParseInt(match, "max"), match.Groups["char"].Value.ToCharArray()[0]);
+
+    return rule.IsValid(match.Groups["password"].Value);
+});
+
+Console.WriteLine($"There are {valid2ndRulePasswords} with the position policy.");
+
+static IEnumerable<string> ReadInLines()
+{
+    using var fileReader = new StreamReader("input.txt");
+    string line;
+    while((line = fileReader.ReadLine()) != null){
+        yield return line;
+    }
+}
+
+static int ParseInt(Match match, string name)
+{
+    return int.Parse(match.Groups[name].Value);
+}
+
+class TimesPasswordPolicy {
+    private int _minOccurences;
+    private int _maxOccurences;
+
+    private char _requieredChar;
+
+    public TimesPasswordPolicy(int minOccurences, int maxOccurences, char requieredChar)
+    {
+        _minOccurences = minOccurences;
+        _maxOccurences = maxOccurences;
+        _requieredChar = requieredChar;
+    }
+
+    public virtual bool IsValid(string password){
+        var occurences = password.ToCharArray().Count(letter => letter == _requieredChar);
+        return occurences >= _minOccurences && occurences <= _maxOccurences;
+    }
+}
+
+class PositionPasswordPolicy {
+    private int _firstPosition;
+    private int _secondPosition;
+
+    private char _requieredChar;
+
+    public PositionPasswordPolicy(int firstPosition, int secondPosition, char requieredChar)
+    {
+        _firstPosition = --firstPosition;
+        _secondPosition = --secondPosition;
+        _requieredChar = requieredChar;
+    }
+
+    public virtual bool IsValid(string password)
+    {
+        var passwordArray = password.ToCharArray();
+        return (HasCharAtPosition(_firstPosition)) ^ HasCharAtPosition(_secondPosition);
+
+        bool HasCharAtPosition(int position)
+        {
+            return passwordArray.Length > position && passwordArray[position] == _requieredChar;
+        }
+    }
+}
