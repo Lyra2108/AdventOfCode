d8f3fdadd40dad78ecec7a89bfe845730510dc01
Sarah Blume <sarah.blume@cas.de>
2021-12-01 17:40:52 +0100

    2021 - Day 1 - Rust

---
+++ 2021/01/aoc01/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn main() {
+    let contents: Vec<i16> = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let task_a: i16 = contents
+        .iter()
+        .skip(1)
+        .zip(contents.iter())
+        .fold(0, |count, touple| {
+            if touple.1 < touple.0 {
+                count + 1
+            } else {
+                count
+            }
+        });
+
+    println!("Task A:\n{}", task_a);
+
+    let task_b: i16 = contents
+        .iter()
+        .skip(3)
+        .zip(contents.iter())
+        .fold(0, |count, touple| {
+            match touple.1 < touple.0
+            {
+                true => count + 1,
+                false => count
+            }
+        });
+
+    println!("Task B:\n{}", task_b);
+}
