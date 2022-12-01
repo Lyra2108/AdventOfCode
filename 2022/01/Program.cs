fc7642df660458104ce0274a37bcdebad7e30cd6
Sarah Blume <sarah.blume@cas.de>
2022-12-01 11:45:07 +0100

    2022-12-01

---
+++ 2022/01/Program.cs
@@ -0,0 +1,33 @@
+﻿var elves = InputElves();
+
+var max = elves
+    .Select(elf => elf.Sum())
+    .Max();
+
+Console.WriteLine($"The max calories are {max}");
+
+var topThree = elves
+    .Select(elf => elf.Sum())
+    .OrderDescending()
+    .ToArray()[..3]
+    .Sum();
+
+Console.WriteLine($"The top three calories are {topThree}");
+
+static IEnumerable<ICollection<int>> InputElves()
+{
+    using var fileReader = new StreamReader("input.txt");
+    string? line;
+
+    var calories = new List<int>();
+    while ((line = fileReader.ReadLine()) != null)
+    {
+        if (line == string.Empty)
+        {
+            yield return calories;
+            calories = new List<int>();
+        }
+        else
+            calories.Add(int.Parse(line));
+    }
+}
