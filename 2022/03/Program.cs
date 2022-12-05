91fe9135e0317a34337586180ff7531b313d8c27
Sarah Blume <sarah.blume@cas.de>
2022-12-05 11:00:31 +0100

    2022-12-03

---
+++ 2022/03/Program.cs
@@ -0,0 +1,37 @@
+﻿var priorities = InputLines()
+    .Select(line =>
+    {
+        var middle = line.Length / 2;
+        return (first: line[..middle], second: line[middle..]);
+    })
+    .Select(compartments => compartments.first.Intersect(compartments.second).First())
+    .Select(ScoreType)
+    .Sum();
+
+Console.WriteLine($"The sum of the priorities is {priorities}");
+
+var groups = InputLines()
+    .Chunk(3)
+    .Select(group => group[0].Intersect(group[1]).Intersect(group[2]).First())
+    .Select(ScoreType)
+    .Sum();
+
+Console.WriteLine($"The groups score is {groups}");
+
+int ScoreType(char type)
+{
+    if (type is >= 'a' and <= 'z')
+        return type - 'a' + 1;
+    return type - 'A' + 27;
+}
+
+static IEnumerable<string> InputLines()
+{
+    using var fileReader = new StreamReader("input.txt");
+    string? line;
+
+    while ((line = fileReader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
