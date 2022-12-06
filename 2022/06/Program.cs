db6bbcb3cf082f8779bc31ad0c228546e8986c38
Sarah Blume <sarah.blume@cas.de>
2022-12-06 14:44:53 +0100

    2022-12-06

---
+++ 2022/06/Program.cs
@@ -0,0 +1,16 @@
+﻿var communication = Communication();
+
+const int chunkLength = 14;
+
+for (var i = chunkLength; i < communication.Length; i++)
+    if (communication[(i - chunkLength)..i].Distinct().Count() == chunkLength)
+    {
+        Console.WriteLine($"The start frame ends at {i}");
+        break;
+    }
+
+static string Communication()
+{
+    using var fileReader = new StreamReader("input.txt");
+    return fileReader.ReadLine()!;
+}
